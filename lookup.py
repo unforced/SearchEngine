b87c40758e60b9ad185e7f3cc7b863ea4fe03147
Aaron Neyer <aaron@aaronneyer.com>
2012-04-03 18:48:36 -0400

    Fixed bug in lookup

--- lookup.py
+++ lookup.py
@@ -1,5 +1,6 @@
 import cPickle
 import sys
+import time
 def find_urls(keyword,index):
 	if keyword in index:
 		return index[keyword]
@@ -10,11 +11,14 @@ def find_all(keywords, index):
 	urls = []
 	for keyword in keywords:
 		new_urls=find_urls(keyword,index)
-		urls = list(set(urls) | set(new_urls))
+		if new_urls:
+			urls = list(set(urls) | set(new_urls))
 	return urls
 
 if __name__ == "__main__":
+	start = time.time()
 	index = cPickle.load(open(sys.argv[1]))
 	urls = find_all(sys.argv[2:],index)
 	for url in urls:
 		print url
+	print "Running time: {}".format(time.time()-start)
